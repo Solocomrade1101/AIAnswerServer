fb6fbcc24ae0f64f104541d0ab47118a9d670214
e_kurbatov <e.kurbatov@smartshell.gg>
2024-09-17 19:54:13 +0300

    update callback

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use(express.static('public'));
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback"
+        callbackURL: "https://aianswerserver-monetize.onrender.com/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
         let user = await User.findOne({ googleId: profile.id });
